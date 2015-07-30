428f733af8f93f235291d048a23791f9275465b2
Kuwamura Hiroyuki <hirokuwamura@gmail.com>
2015-07-30 11:37:06 +0900

    to write ReadMe.md

---
+++ ReadMe.md
@@ -0,0 +1,32 @@
+#Find-Unimg
+
+## What is this?
+***
+
+These code can search to no image files used in website date(http directory).
+It need to use gulp.
+
+
+## Why make it?
+***
+
+It keep clean in dir.
+
+
+##Usage
+***
+Pls do on your local.
+
+These can't search to dynamical change image files.
+
+ex. When we hover image that is name hoge-(on|over).png or hoge-(off|out).png.
+
+I'm going to solve these problem by making igonorefile of search. 
+
+
+
+## Help 
+***
+I'm not good at js programming and node.js.
+
+so that, Plaese inform me some idea of performance.
--- gulpfile.js
+++ gulpfile.js
@@ -4,28 +4,25 @@ var finf = require('find-in-files');
 var glob = require("glob");
 var path = require('path');
 var date = require('date-utils');
-
 var paths = {
     "rootDir": "_dev/",
     "targetSrc": ".(php|css)$",
     "imgSrc": "_dev/img/**/*",
     "logData": "log/"
 };
-var dt = new Date();
-var formatted = dt.toFormat("YYYYMMDDHH24MISS");
-
 gulp.task('findUnimg',function(){
+    var dt = new Date();
+    var formatted = dt.toFormat("YYYYMMDDHH24MISS");
     var pattern = paths.imgSrc;
     glob(pattern, function (err, files) {
         if(err) {
             console.log(err);
         }
         files.forEach(function(file){
-            var flag = 0;
             var filename = path.basename(file);
             finf.find(filename,paths.rootDir,paths.targetSrc)
                 .then(function(results){
-                    if(!(Object.keys(results).length > 0)){
+                    if(Object.keys(results).length <= 0){
                         fs.appendFile(paths.logData+'log'+formatted+'.log', file+"\n" ,'utf8');
                         console.log(file);
                     }
